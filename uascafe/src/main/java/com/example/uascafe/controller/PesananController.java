775986ff72901bfca69f5e8fffddf19a69fae3d8
liudaniel2201@gmail.com <liudaniel2201@gmail.com>
2024-11-25 20:26:16 +0700

    up to date 4

--- uascafe/src/main/java/com/example/uascafe/controller/PesananController.java
+++ uascafe/src/main/java/com/example/uascafe/controller/PesananController.java
@@ -46,41 +46,6 @@ public class PesananController {
         return "redirect:/pesanan";
     }
 
-    // // Menampilkan form untuk membuat pesanan baru
-    // @GetMapping("/pesanan/create")
-    // public String createPesananForm(Model model) {
-    //     model.addAttribute("pesanan", new Pesanan());
-    //     // Menampilkan daftar menu dan pembayaran untuk dipilih
-    //     model.addAttribute("menuList", menuService.getAllMenu());
-    //     model.addAttribute("pembayaranList", pembayaranService.getAllPembayaran());
-    //     return "Dashboard/createPesanan";
-    // }
-
-    // // Menyimpan pesanan baru
-    // @PostMapping("/pesanan/save")
-    // public String savePesanan(
-    //         @ModelAttribute Pesanan pesanan,
-    //         @RequestParam("menuId") int menuId,
-    //         @RequestParam("pembayaranId") int pembayaranId,
-    //         Model model) {
-    //     // Mendapatkan Menu dan Pembayaran berdasarkan ID
-    //     Optional<Menu> menu = menuService.getMenuById(menuId);
-    //     Optional<Pembayaran> pembayaran = pembayaranService.getPembayaranById(pembayaranId);
-
-    //     if (menu.isPresent() && pembayaran.isPresent()) {
-    //         pesanan.setMenu(menu.get());
-    //         pesanan.setPembayaran(pembayaran.get());
-    //         pesanan.setTanggal(LocalDateTime.now()); // Tetapkan tanggal otomatis di backend
-    //         pesananService.createPesanan(pesanan);
-    //         return "redirect:/pesanan";
-    //     }
-
-    //     model.addAttribute("errorMessage", "Menu atau Pembayaran tidak ditemukan");
-    //     model.addAttribute("menuList", menuService.getAllMenu());
-    //     model.addAttribute("pembayaranList", pembayaranService.getAllPembayaran());
-    //     return "Dashboard/createPesanan";
-    // }
-
     // Menghapus pesanan
     @GetMapping("/pesanan/delete/{id}")
     public String deletePesanan(@PathVariable("id") int id, Model model) {
